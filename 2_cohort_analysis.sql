88558c3eb928f74114268646726ee343c6314837
varshi_space <varshithab523@gmail.com>
2026-08-16 18:10:52 +0530

    Complete SQL sales analysis project

---
+++ 2_cohort_analysis.sql
@@ -0,0 +1,9 @@
+SELECT
+    cohort_year,
+    SUM(total_net_revenue) AS total_revenue,
+    COUNT(DISTINCT customerkey) AS total_customers,
+    SUM(total_net_revenue) / COUNT(DISTINCT customerkey) AS customer_revenue
+FROM cohort_analysis
+WHERE orderdate = first_purchase_date
+GROUP BY 
+    cohort_year
